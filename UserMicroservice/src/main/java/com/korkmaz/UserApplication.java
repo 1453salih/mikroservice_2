c993efb28d4d5706fcd79f9d9ca6a74cf0c23186
Salih korkmaz <salihkorkmaz108478@gmail.com>
2024-11-15 00:43:30 +0300

    fc

---
+++ UserMicroservice/src/main/java/com/korkmaz/UserApplication.java
@@ -0,0 +1,9 @@
+package com.korkmaz;
+
+import org.springframework.boot.SpringApplication;
+
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
